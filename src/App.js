be34070dfaa00af7a71a82ceed662f0f35e5ac75
noomgeo <noammizrahi@hotmail.com>
2019-06-23 16:11:30 +0300

    built chartts api connections

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CoinPicker from './CoinPicker';
+import RatesChart from './RatesChart';
 
-function App() {
+class App extends React.Component {
+  state = {
+    fromCoin: '',
+    toCoin: '',
+    historicalRates: [],
+  }
+
+setFromCoin = (event)=> {
+  this.setState({
+    fromCoin: event.target.value,
+  })
+}
+
+setToCoin = (event)=> {
+  this.setState({
+    toCoin: event.target.value,
+  })
+}
+
+componentDidUpdate(prevProps, prevState){
+  if( this.state.toCoin && this.state.fromCoin &&
+      ( (this.state.fromCoin !== prevState.fromCoin ) ||
+        (this.state.toCoin !== prevState.toCoin ) ) ) {
+
+    fetch(`https://min-api.cryptocompare.com/data/histoday?fsym=${this.state.fromCoin}&tsym=${this.state.toCoin}&limit=60`)
+      .then(response=>response.json())
+      .then(responseJson=>{
+        console.log(responseJson)
+        this.setState({historicalRates: responseJson.Data});
+      });
+    }
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CoinPicker fromCoin={this.state.fromCoin}
+                  setFromCoin={this.setFromCoin}
+                  toCoin={this.state.toCoin}
+                  setToCoin={this.setToCoin}/>
+      <RatesChart rates={this.state.historicalRates}/>
     </div>
   );
+  }
 }
 
 export default App;
